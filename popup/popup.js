e36c45b076a196b2f0bad4a88dab5926bc4dcdc1
Monixc <chris13404@gmail.com>
2024-02-17 02:16:02 +0900

    [Feat] 색 변경 초기화

--- popup/popup.js
+++ popup/popup.js
@@ -9,6 +9,15 @@ document
     });
   });
 
+document.getElementById("loader-button").addEventListener("click", function () {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    chrome.scripting.executeScript({
+      target: { tabId: tabs[0].id },
+      function: resetColor,
+    });
+  });
+});
+
 function changeColor() {
   let comments = document.querySelectorAll("ytd-comment-renderer");
 
@@ -16,3 +25,17 @@ function changeColor() {
     comment.style.backgroundColor = "#FFD600";
   });
 }
+
+function resetColor() {
+  let comments = document.querySelectorAll("ytd-comment-renderer");
+
+  comments.forEach((comment) => {
+    comment.style.backgroundColor = "";
+  });
+}
+
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  if (request.action === "changeColor") {
+    changeColor();
+  }
+});
