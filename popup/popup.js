bdbd32cb05937fdca639dfe0a9bf60693d8169bc
Monixc <chris13404@gmail.com>
2024-02-21 19:29:23 +0900

    [Fix] reset 후 whisper 시 색상 미변환 오류 해결

--- popup/popup.js
+++ popup/popup.js
@@ -28,20 +28,30 @@ function handleButton(userNames, color) {
 
 // 초기화 함수 정의
 function resetColors() {
-  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-    chrome.scripting.executeScript({
-      target: { tabId: tabs[0].id },
-      function: function () {
-        let comments = document.querySelectorAll("ytd-comment-renderer");
-        // 모든 댓글의 배경색을 초기화
-        comments.forEach((comment) => {
-          comment.style.backgroundColor = "white";
-        });
-      },
+  return new Promise((resolve, reject) => {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      chrome.scripting.executeScript(
+        {
+          target: { tabId: tabs[0].id },
+          function: function () {
+            let comments = document.querySelectorAll("ytd-comment-renderer");
+            // 모든 댓글의 배경색을 초기화
+            comments.forEach((comment) => {
+              comment.style.backgroundColor = "white";
+            });
+          },
+        },
+        () => {
+          if (chrome.runtime.lastError) {
+            reject(chrome.runtime.lastError);
+          } else {
+            resolve();
+          }
+        }
+      );
     });
   });
 }
-
 //말풍선 이미지 변경
 let image0 = document.getElementById("image0");
 let image1 = document.getElementById("image1");
@@ -125,12 +135,11 @@ document
           document.getElementById("whisper-button").innerHTML =
             "Do whisper<div class='loader'></div>";
           document.querySelector(".loader").style.display = "none";
-          isWaiting = false; // 'Waiting for response' 상태가 끝난 후에 isWaiting을 false로 설정
-          index = 6; // 'Waiting for response' 상태가 끝난 후에 index를 6으로 설정
+          isWaiting = false;
+          index = 6;
           rotateImages();
 
           setTimeout(() => {
-            // 일정 시간 후에 index를 0으로 설정
             index = 0;
             rotateImages();
           }, 5000);
@@ -147,24 +156,25 @@ document
           return response.json();
         })
         .then((data) => {
-          console.log(data);
-          if (Array.isArray(data.data)) {
-            data.data.forEach((item) => {
-              if (item.color != null) {
-                handleButton([item.author], item.color);
-              } else {
-                console.error(
-                  "Color is null or undefined for author",
-                  item.author
-                );
-              }
-            });
-          } else {
-            console.log("The response.data is not an array.");
-            isError = true;
-            index = 3;
-            rotateImages();
-          }
+          resetColors().then(() => {
+            if (Array.isArray(data.data)) {
+              data.data.forEach((item) => {
+                if (item.color != null) {
+                  handleButton([item.author], item.color);
+                } else {
+                  console.error(
+                    "Color is null or undefined for author",
+                    item.author
+                  );
+                }
+              });
+            } else {
+              console.log("The response.data is not an array.");
+              isError = true;
+              index = 3;
+              rotateImages();
+            }
+          });
         })
         .catch((error) => {
           document.getElementById("whisper-button").innerHTML =
@@ -183,9 +193,10 @@ document
 
 // reset-button 클릭 이벤트에 핸들러 연결
 document.getElementById("reset-button").addEventListener("click", function () {
-  resetColors();
-  index = 7;
-  rotateImages();
+  resetColors().then(() => {
+    index = 7;
+    rotateImages();
+  });
 });
 
 // 페이지가 로드되면 이미지 클릭 이벤트에 핸들러 연결
